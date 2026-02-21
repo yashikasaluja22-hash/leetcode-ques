ed646e5edc5b935e351b8187ff87bccc98da963f
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-02-21 21:08:07 +0530

    new refernce

---
+++ link.cpp
@@ -0,0 +1,13 @@
+/* If you are a beginner solve these problems which makes concepts clear for future coding:
+
+Two Sum
+Roman to Integer
+Palindrome Number
+Maximum Subarray
+Remove Element
+Contains Duplicate
+Add Two Numbers
+Majority Element
+Remove Duplicates from Sorted Array
+Practice them in a row for better understanding and please Upvote the post for more questions.
+on leetcode */
